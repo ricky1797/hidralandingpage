db6bd3189d481b89ce85c3c1fb958ab40b87e88a
Ricardo Mejia <rickym1497@gmail.com>
2024-03-05 12:19:57 -0800

    build

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import logo from './img/Hidralogo.png'
+
+function App() {
+  return (
+    <div className="coming-soon">
+      <img src={logo} className='hidralogo'/>
+    <h1>Coming Soon!</h1>
+    <p className='p-tag'>We're working hard to bring you something amazing. Stay tuned!</p>
+  </div>
+  );
+}
+
+export default App;
